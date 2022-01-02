42298cd737165976fd0181203852d2f7872d2890
Mohamed Mostafa <72346762+MMostafa-Hub@users.noreply.github.com>
2022-01-02 12:57:37 +0200

    Refactoring and adding a visualization to the path

--- PathPlanning/Travelling.py
+++ PathPlanning/Travelling.py
@@ -1,16 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import argmax
 
-alpha = 1.0
-beta = 1.0
-
-# graph_points = np.array(
-#     [[0.,    0.],
-#      [114.28714842,   41.98759603],
-#      [62.47783741,  -10.16037304],
-#      [24.5058101,  179.34217451],
-#      [-9.51912637,   86.66461683]
-#      ]
-# )
 graph_points = np.array(
     [[0.,    0.],
      [114.28714842,   41.98759603],
@@ -34,27 +25,67 @@ graph_points = np.array(
      [95.02087717,   63.66352072]]
 )
 
-
 n_nodes = graph_points.shape[0]
 prob_matrix = np.zeros((n_nodes, n_nodes))
 pheremone_matrix = np.zeros((n_nodes, n_nodes))
+distance_matrix = np.zeros((n_nodes, n_nodes))
 
 
-def get_max_prob_index(root, visited, beta=1, alpha=1):
-    return np.argmax(np.multiply(np.power(pheremone_matrix[root], alpha), np.power(np.logical_xor(visited, 1), beta)))
+def fill_distance_matrix():
+    # Just fills the cost matrix between each node
+
+    for i in range(n_nodes):
+        for j in range(n_nodes):
+            distance_matrix[i][j] = np.linalg.norm(
+                graph_points[i] - graph_points[j])
 
 
 def dist(point_a, point_b):
+    # returns the Euclidian distance between two points
     return np.linalg.norm(point_a - point_b)
 
 
-def update_pheremones(distances_travelled, paths, Q=100, row=.5):
+def get_max_prob_index(root_index, visited, beta=2, alpha=1):
+
+    # list contains the probability of each unvisited node
+    probs = []
+
+    # we want the pheremones of the nodes we'didnt visit yet
+    root_pheremones = np.multiply(
+        pheremone_matrix[root_index], np.logical_xor(visited, 1))
+
+    # if all the pheremones are zeros,it means that we are still in the first eteration
+    if np.all(root_pheremones == 0):
+        return -1
+
+    # pher ** alpha
+    pher_pow_alpha = np.power(root_pheremones, alpha)
+
+    # ( 1 / dist ) ** beta
+    dists_pow_beta = np.power(np.divide(1, distance_matrix[root_index]), beta)
+
+    # since the dist can be zero so 1/dist can be inf , so we replace it with 0
+    dists_pow_beta[dists_pow_beta == np.inf] = 0
+
+    # sigma of all unvisited nodes = (pher_i ** alpha) * ((1 / dists_i) ** beta)
+    s = np.sum(np.multiply(pher_pow_alpha, dists_pow_beta))
+
+    # filling the probability list
+    for pher, dist in zip(pher_pow_alpha, dists_pow_beta):
+        probs.append((pher * dist) / s)
+
+    # retunring the index of the max probability
+    return probs.index(max(probs))
+
+
+def update_pheremones(distances_travelled, paths, Q=100, row=.9):
     # k ants
     for k in range(len(paths)):
         for i in range(len(paths[k]) - 1):
             node_i = paths[k][i]
             after_node_i = paths[k][i+1]
 
+            # pher_i_j = Q / distance travelled_k
             # as the graph is undirceted
             pheremone_matrix[node_i][after_node_i] = row * \
                 pheremone_matrix[node_i][after_node_i] + \
@@ -65,9 +96,10 @@ def update_pheremones(distances_travelled, paths, Q=100, row=.5):
 
 
 def ACO():
-    n_iterations = 50
-    k_ants = 50
-
+    fill_distance_matrix()
+    
+    n_iterations = n_nodes + 1
+    k_ants = n_nodes
     for i in range(n_iterations):
 
         # to store the total pherimones secreted by k_ants
@@ -95,15 +127,16 @@ def ACO():
             neighbour_nodes = np.delete(graph_points, root_index, 0)
 
             # add the root node to the path
-            # path_nodes = np.array([root])
             path_indices = [root_index]
 
             iterator_root = root
             iterator_root_index = root_index
             while not np.all(visited == True):
 
+                max_prob_index = get_max_prob_index(
+                    iterator_root_index, visited)
                 # if it's the first iteration
-                if get_max_prob_index(iterator_root_index, visited) == 0:
+                if max_prob_index == -1:
                     # a random root choosen from neighbour_nodes
                     # which is garanteed to be not visited
                     new_root = np.random.default_rng().choice(neighbour_nodes, 1, replace=False)
@@ -114,9 +147,8 @@ def ACO():
 
                 # if it's not the first iteration
                 else:
-                    # making the next root with maximum prob in the probability matrix
-                    new_root_index = get_max_prob_index(
-                        iterator_root_index, visited)
+                    # making the next root with maximum prob in the probability list
+                    new_root_index = max_prob_index
                     new_root = graph_points[new_root_index]
 
                 # The new_root is marked as visited
@@ -131,14 +163,10 @@ def ACO():
                     neighbour_nodes, new_root_index_neighbour, 0)
 
                 # add the new_root node to the path
-                # path_nodes = np.vstack([path_nodes, new_root])
                 path_indices.append(new_root_index)
 
                 # distance travelled by the ant
-                distance = dist(iterator_root, new_root)
-                distance_travelled += distance
-                # print(
-                #     f"the distance from {iterator_root_index} to {new_root_index} is {distance}")
+                distance_travelled += distance_matrix[iterator_root_index][new_root_index]
 
                 # Iterating
                 iterator_root = new_root
@@ -148,7 +176,7 @@ def ACO():
             path_indices.append(root_index)
 
             # adding the distance from the last node to the root
-            distance_travelled += dist(iterator_root, root)
+            distance_travelled += distance_matrix[iterator_root_index][root_index]
 
             # Update the paths
             paths.append(path_indices)
@@ -161,7 +189,15 @@ def ACO():
 
         # update the pheremone Matrix
         update_pheremones(distances_travelled, paths)
+        print(pheremone_matrix)
+    return paths[0]
 
 
-ACO()
-print(pheremone_matrix)
+# scatter plot of the graph points
+plt.scatter(graph_points[:, 0], graph_points[:, 1])
+
+# the best path from point 0,0 to point 0,0
+plan = np.array([graph_points[i] for i in ACO()])
+plt.plot(plan[:, 0], plan[:, 1])
+
+plt.show()
